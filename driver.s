71d7ec6a4679e48a487952b822c4759a2d52a771
natashajyw <wu.tasha88@gmail.com>
2023-10-14 11:49:03 -0700

    string_charAt worksgit add * Might consider adding error checking in the future (side note: ANDREW IS THE GOAT)

--- driver.s
+++ driver.s
@@ -3,16 +3,20 @@ szS1:	.asciz	"Cat in the hat."
 szS2:	.asciz	"Green eggs and ham."
 szS3:	.asciz	"cat in the hat."
 
-szS1Length:		.asciz "s1.length() = "
-szS2Length:		.asciz "\ns2.length() = "
-szS3Length:		.asciz "\ns3.length() = "
-szS1S3Equals:	.asciz "\nString_equals(s1,s3) = "
-szS1S1Equals:	.asciz "\nString_equals(s1,s1) = "
+szS1Length:			.asciz "s1.length() = "
+szS2Length:			.asciz "\ns2.length() = "
+szS3Length:			.asciz "\ns3.length() = "
+szS1S3Equals:		.asciz "\nString_equals(s1,s3) = "
+szS1S1Equals:		.asciz "\nString_equals(s1,s1) = "
+szS1S3EqualsIgn:	.asciz "\nString_equalsIgnoreCase(s1,s3) = "
+szS1S2EqualsIgn:	.asciz "\nString_equalsIgnoreCase(s1,s2) = "
+szCharAt:			.asciz "\nString_charAt(s2,4) = "
 
 //temp
 szTrue:		.asciz "TRUE"
 szFalse:	.asciz "FALSE"
 
+szChar:		.skip 1
 szBool:		.skip 1
 szSkip:		.skip 20
 chEndl:		.byte 10
@@ -74,16 +78,37 @@ _start:
 	bl	String_equals		// branch and link function String_equals
 	//b	booltoasc			// branch and link function booltoasc
 	
-	cmp x0,	#1
-	bne false
+//temp
+	//cmp x0,	#1
+	//bne false
 	
-	ldr x0,=szTrue
-	bl putstring
-	b exit
+	//ldr x0,=szTrue
+	//bl putstring
+	//b exit
 	
-false:
-	ldr x0,=szFalse
-	bl	putstring
+//false:
+	//ldr x0,=szFalse
+	//bl	putstring
+	
+	
+// ========================== String_equalsIgnoreCase ========================== //
+	
+// ========================== String_charAt ========================== //
+	ldr x0, =szCharAt	// loads address of szCharAt into x0
+	bl	putstring		// branch and link to function putstring
+	
+	ldr x0, =szS2		// loads address of szS2 into x0
+	mov x1, #4			// moves the value 4 into x1
+	bl	String_charAt	// branch and link to function String_charAt
+	
+	// THANK YOU ANDREW <33333 ALL HAIL KING ANDREW
+	ldr  x1, =szChar	// loads address of szChar into x1
+	strb w0, [x1]		// stores byte in w0 into address in x1
+	
+	ldr x0, =szChar		// loads address of szChar into x0
+	bl	putch			// branch and link to function putch
+	
+
 	
 exit:
 	mov x0, #0		// use 0 return code
